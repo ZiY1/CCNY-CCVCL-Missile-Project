7af4ca7b663b816345c4bcc3d28a6e605aefc1e9
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2021-08-24 21:46:19 -0400

    added rotation randomizer interface

--- Assets/PerceptionInterface.cs
+++ Assets/PerceptionInterface.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 using UnityEngine.Perception.Randomization.Scenarios;
 using UnityEngine.UI;
 using Unity.Simulation;
+using UnityEngine.Perception.Randomization.Randomizers.SampleRandomizers;
+using UnityEngine.Perception.Randomization.Parameters;
+using UnityEngine.Perception.Randomization.Samplers;
 
 public class PerceptionInterface : MonoBehaviour
 {
     public FixedLengthScenario fls;
 
     public Text seedText;
+
     public Text totalIterationsText;
 
+    public Text rotRandXMin_Text;
+    public Text rotRandYMin_Text;
+    public Text rotRandZMin_Text;
+
+    public Text rotRandXMax_Text;
+    public Text rotRandYMax_Text;
+    public Text rotRandZMax_Text;
+
+    public Text dataSetDirectoyPath_Text;
+    public Text copyPathButton_Text;
+
     public InputField seedInputfield;
+
     public InputField totalIterationsInputfield;
+
+    public InputField rotRandXmin_Inputfield;
+    public InputField rotRandXmax_Inputfield;
+
+    public InputField rotRandYmin_Inputfield;
+    public InputField rotRandYmax_Inputfield;
+
+    public InputField rotRandZmin_Inputfield;
+    public InputField rotRandZmax_Inputfield;
 
     uint newSeed;
     int newTotalIterations;
+
+    float min_x = -10;
+    float max_x = 10;
+    float min_y = -10;
+    float max_y = 10;
+    float min_z = -10;
+    float max_z = 10;
+
+    float orig_min_x, orig_max_x, orig_min_y, orig_max_y, orig_min_z, orig_max_z;
 
     public RawImage raw;
     public Camera cam;
@@ -32,10 +66,23 @@ public class PerceptionInterface : MonoBehaviour
     {
         // sets folder where the dataset will be stored
         Configuration.localPersistentDataPath = Application.persistentDataPath;
+
+        dataSetDirectoyPath_Text.text = Configuration.localPersistentDataPath;
         //Debug.Log(Application.persistentDataPath + "/Datasets");
 
         seedText.text = "\tSeed: " + fls.constants.randomSeed.ToString();
         totalIterationsText.text = "\tTotal Iterations: " + fls.constants.totalIterations.ToString();
+
+        rotRandXMin_Text.text = "\tX: min " + min_x.ToString(); rotRandXMax_Text.text = "\tmax " + max_x.ToString();
+        rotRandYMin_Text.text = "\tY: min " + min_y.ToString(); rotRandYMax_Text.text = "\tmax " + max_y.ToString();
+        rotRandZMin_Text.text = "\tZ: min " + min_z.ToString(); rotRandZMax_Text.text = "\tmax " + max_z.ToString();
+
+        orig_min_x = min_x;
+        orig_max_x = max_x;
+        orig_min_y = min_y;
+        orig_max_y = max_y;
+        orig_min_z = min_z;
+        orig_max_z = max_z;
     }
 
     // Function to open the scenario editing interface
@@ -46,6 +93,22 @@ public class PerceptionInterface : MonoBehaviour
 
         seedInputfield.text = newSeed.ToString();
         totalIterationsInputfield.text = newTotalIterations.ToString();
+
+        min_x = orig_min_x;
+        min_y = orig_min_y;
+        min_z = orig_min_z;
+
+        max_x = orig_max_x;
+        max_y = orig_max_y;
+        max_z = orig_max_z;
+
+        rotRandXmin_Inputfield.text = min_x.ToString();
+        rotRandYmin_Inputfield.text = min_y.ToString();
+        rotRandZmin_Inputfield.text = min_z.ToString();
+
+        rotRandXmax_Inputfield.text = max_x.ToString();
+        rotRandYmax_Inputfield.text = max_y.ToString();
+        rotRandZmax_Inputfield.text = max_z.ToString();
     }
 
     // Function to read input for the scenario seed from the input field
@@ -70,12 +133,63 @@ public class PerceptionInterface : MonoBehaviour
 
         seedText.text = "\tSeed: " + fls.constants.randomSeed.ToString();
         totalIterationsText.text = "\tTotal Iterations: " + fls.constants.totalIterations.ToString();
+
+        fls.GetRandomizer<CustomRotationRandomizer>().rotation = new Vector3Parameter
+        {
+            x = new UniformSampler(min_x, max_x),
+            y = new UniformSampler(min_y, max_y),
+            z = new UniformSampler(min_z, max_z)
+        };
+
+        rotRandXMin_Text.text = "\tX: min " + min_x.ToString(); rotRandXMax_Text.text = "\tmax " + max_x.ToString();
+        rotRandYMin_Text.text = "\tY: min " + min_y.ToString(); rotRandYMax_Text.text = "\tmax " + max_y.ToString();
+        rotRandZMin_Text.text = "\tZ: min " + min_z.ToString(); rotRandZMax_Text.text = "\tmax " + max_z.ToString();
+
+        orig_min_x = min_x;
+        orig_max_x = max_x;
+        orig_min_y = min_y;
+        orig_max_y = max_y;
+        orig_min_z = min_z;
+        orig_max_z = max_z;
     }
 
-    // Function to open file explorer where the folder containing the datasets is located
+    // Function to copy path where the folder containing the datasets is located
     public void ShowDatasetsFolder()
     {
-        UnityEditor.EditorUtility.RevealInFinder(Application.persistentDataPath + "/.");
+        GUIUtility.systemCopyBuffer = Configuration.localPersistentDataPath;
+        dataSetDirectoyPath_Text.text = Configuration.localPersistentDataPath;
+
+        StopCoroutine("copyPathButtonTextChange");
+        StartCoroutine("copyPathButtonTextChange");
+    }
+
+    IEnumerator copyPathButtonTextChange()
+    {
+        copyPathButton_Text.text = "Copied!";
+        yield return new WaitForSeconds(5);
+        copyPathButton_Text.text = "Copy path";
+
+    }
+
+    // Function to change the RotatinRandomizer x,y, and z values
+    public void changeRotRandVals()
+    {
+        float.TryParse(rotRandXmin_Inputfield.text, out float new_minX);
+        float.TryParse(rotRandYmin_Inputfield.text, out float new_minY);
+        float.TryParse(rotRandZmin_Inputfield.text, out float new_minZ);
+
+        float.TryParse(rotRandXmax_Inputfield.text, out float new_maxX);
+        float.TryParse(rotRandYmax_Inputfield.text, out float new_maxY);
+        float.TryParse(rotRandZmax_Inputfield.text, out float new_maxZ);
+
+        min_x = new_minX;
+        min_y = new_minY;
+        min_z = new_minZ;
+
+        max_x = new_maxX;
+        max_y = new_maxY;
+        max_z = new_maxZ;
+
     }
 
 }
--- Assets/RocketTest/Script/AngleOutput.cs
+++ Assets/RocketTest/Script/AngleOutput.cs
@@ -11,6 +11,6 @@ public class AngleOutput : MonoBehaviour
     void Update()
     {
         //Debug.Log(string.Format("Parent Target: {0}, \nChild Target: {0}", target01.transform.eulerAngles, target02.transform.eulerAngles));
-        Debug.Log(target01.name+  ": " + target01.transform.eulerAngles + "\n" + target02.name + ": " + target02.transform.eulerAngles);
+        //Debug.Log(target01.name+  ": " + target01.transform.eulerAngles + "\n" + target02.name + ": " + target02.transform.eulerAngles);
     }
 }
