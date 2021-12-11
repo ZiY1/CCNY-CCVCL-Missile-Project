96110305f43c649885475cbcab76d5703c6609b7
ZiY1 <hzy1091149656@gmail.com>
2021-12-10 23:19:34 -0500

    Shape Prior Program Perception Fix
    Fixed the perception accordingly.

---
+++ Assets/RocketTest/ShapePrior/Script/ShapePriorStartPerception.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Perception.GroundTruth;
+using UnityEngine.UI;
+using UnityEngine.Perception.Randomization.Scenarios;
+
+public class ShapePriorStartPerception : MonoBehaviour
+{
+    //public CameraManager cm;
+
+    public PerceptionCamera pc;
+    public FixedLengthScenario fls;
+    public CustomAnnotationAndMetricReporter cus;
+
+    private void Awake()
+    {
+        pc.enabled = false;
+        cus.enabled = false;
+        fls.enabled = false;
+    }
+
+    public void BeginCapture()
+    {
+        Debug.Log("button pressed");
+        pc.enabled = true;
+        cus.enabled = true;
+        fls.enabled = true;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //cm.LockOnToObject();
+
+        if (pc.enabled)
+        {
+            pc.RequestCapture();
+        }
+
+        if (cus.enabled)
+        {
+            cus.CaptureCustomData();
+        }
+    }
+}
+
